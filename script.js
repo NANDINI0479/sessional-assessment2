415d4f5a25add27de20ae097841a18ae2eda248e
NANDINI0479 <nandini0479.be20@chitkara.edu.in>
2022-08-11 21:46:07 +0530

    first commit

--- script.js
+++ script.js
@@ -29,3 +29,30 @@ console.log(output)
 
     xhr.send()
 })
+// var btn=document.getElementById("btn").addEventListener("click",getPost);
+// var con=0;
+// var div=document.getElementById("cardDiv")
+
+// function getPost(){
+//     fetch('https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09')
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((post)=>{
+//         for(let index = 0;index<8;index++){
+//             div.innerHTML+=`
+//             <div class=card col-3 m-1 mx-auto">
+//             <img class=card-img-top" src=${post[con].titleUrl}">
+//             <div class=card-body">
+//             <h5 class="card-title">${post[con].id}</h5>
+//             <p class="card-text">${post[con].body}</p>
+//             </div>
+//             </div>`
+
+//             con=con+1;
+//         }
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     })
+// }
